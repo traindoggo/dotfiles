65beb1909bd08cafdb99a5acf9b3b1e892d5358e
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-20 20:10:54 +0900

    compe temper

---
+++ compe_cpp/solve.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef DEBUG_
+#include <compe/debug.hpp>
+#else
+#define dump(...)
+#endif
+
+// clang-format off
+struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
+
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
+#define el '\n'
+
+#define all(k)  k.begin(),  k.end()
+#define rall(k) k.rbegin(), k.rend()
+
+#define INFi  1   << 30
+#define INFll 1LL << 60
+#define MOD17 10'0000'0007
+#define MOD98  9'9824'4353
+
+template <typename T> inline bool chmax(T& a, const T& b) {
+  return ((a < b) ? (a = b, true) : false);
+}
+template <typename T> inline bool chmin(T& a, const T& b) {
+  return ((a > b) ? (a = b, true) : false);
+}
+
+using ll = long long int;
+// clang-format on
+
+int main() {
+  ;
+}
