96c54f51837d5c8906d95db6fd2579f29f473077
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-05-20 20:11:01 +0900

    new copy

--- copy.sh
+++ copy.sh
@@ -3,9 +3,10 @@ cp ~/.vimrc .
 cp ~/.bashrc .
 cp ~/.zshrc .
 cp ~/.config/alacritty/alacritty.toml .
+cp ~/.config/temper/compe_cpp . -r
 
 mkdir nvim
-cp ~/.config/nvim/init.lua ./nvim/
-cp ~/.config/nvim/lua ./nvim/lua/ -r
-cp ~/.config/nvim/snippets ./nvim/ -r
+cp ~/.config/nvim/init.lua ./nvim
+cp ~/.config/nvim/lua ./nvim -r
+cp ~/.config/nvim/snippets ./nvim -r
 rm ./nvim/lua/.luarc.json
