3dabde6b3b65c09a46cf5346afbf276755cde62a
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-06-01 02:43:01 +0900

    new compe_cpp

--- compe_cpp/solve.cc
+++ compe_cpp/solve.cc
@@ -10,13 +10,12 @@ using namespace std;
 // clang-format off
 struct  Fast{Fast(){std::cin.tie(0);ios::sync_with_stdio(false);}} fast;
 
-#define rep(i,n) for (int i=0;i<(int)n;++i)
-#define die(msg) do{cout<<(msg)<<'\n',exit(0);}while(0)
 #define el '\n'
+#define rep(i,n) for (int i=0;i<(int)n;++i)
+#define die(msg) do{cout<<(msg)<<el;exit(0);}while(0);
 
 #define all(k)  k.begin(),  k.end()
 #define rall(k) k.rbegin(), k.rend()
-
 #define INFi  1   << 30
 #define INFll 1LL << 60
 #define MOD17 10'0000'0007
